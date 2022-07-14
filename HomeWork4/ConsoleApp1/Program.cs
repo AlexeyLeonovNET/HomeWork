30d5360843682c671f84ab7bf8ab8797167e6b7b
Alexey Leonov <x1leon1x@gmail.com>
2022-07-14 21:43:34 +0300

    HomeWork

---
+++ HomeWork4/ConsoleApp1/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+//Леонов Алексей Николаевич
+
+//Решить задачу с логинами из урока 2, только логины и пароли считать из файла в массив. Создайте структуру Account, содержащую Login и Password.
+//*б) Добавить конструктор и методы, которые загружают данные из файла и записывают данные в файл.
+//**в) Обработать возможные исключительные ситуации при работе с файлами.
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(Verify(@"D:\test.txt"));
+            Console.ReadKey();
+        }
+
+        /// <summary>
+        /// Читает с текстового файла
+        /// </summary>
+        /// <param name="filename"></param>
+        /// <returns></returns>
+        static bool Verify(string filename)
+        {
+            if (File.Exists(filename))
+            {
+                StreamReader sr = new StreamReader(filename);
+                string[] a = new string[2];
+                a[0] = sr.ReadLine();
+                a[1] = sr.ReadLine();
+                sr.Close();
+
+                if (a[0] == "root" && a[1] == "GeekBrains")
+                {
+                    return true;
+                }
+                else
+                {
+                    return false;
+                }
+            }
+            else
+            {
+                Console.WriteLine("Error load file");
+                return false;
+            }            
+        }
+    }
+}
---
+++ HomeWork5/Task_3/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+// Леонов Алексей Николаевич
+
+//3. *Для двух строк написать метод, определяющий, является ли одна строка перестановкой другой.
+// Например: badc являются перестановкой abcd.
+
+namespace Task_3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(Compare_2_words("badc", "abcd"));
+            Console.WriteLine(Compare_2_words("badc", "abcde"));
+            Console.WriteLine(Compare_2_words("badc", "asds"));
+            Console.ReadKey();
+        }
+
+        static bool Compare_2_words (string word1, string word2)
+        {
+            if (word1.Length == word2.Length)
+            {
+                char[] array1 = word1.ToCharArray();
+                char[] array2 = word2.ToCharArray();
+                Array.Sort(array1);
+                Array.Sort(array2);
+                return Enumerable.SequenceEqual(array1, array2);                
+            }
+            return false;
+        }
+    }
+}
